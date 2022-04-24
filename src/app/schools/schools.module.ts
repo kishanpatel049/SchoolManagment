7fe63e70c8a07414748a9e6fb359f0e5798cfa9e
kishanpatel049 <99271404+kishanpatel049@users.noreply.github.com>
2022-04-24 08:38:57 -0400

    created components

---
+++ src/app/schools/schools.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SchoolsRoutingModule } from './schools-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SchoolsRoutingModule
+  ]
+})
+export class SchoolsModule { }
---
+++ src/app/semesters/semesters.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SemestersRoutingModule } from './semesters-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SemestersRoutingModule
+  ]
+})
+export class SemestersModule { }
